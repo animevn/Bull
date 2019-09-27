7148ab65746c8883bd7b2b0e6b15291df13ce038
animevn <haanhth21@gmail.com>
2019-09-27 09:28:10 +0700

    create layouts

--- Bull/ViewController.swift
+++ Bull/ViewController.swift
@@ -1,10 +1,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var lbTarget: UILabel!
+    @IBOutlet weak var lbMin: UILabel!
+    @IBOutlet weak var lbMax: UILabel!
+    @IBOutlet weak var sdSlider: UISlider!
+    @IBOutlet weak var lbInfo: UILabel!
+    @IBOutlet weak var lbScore: UILabel!
+    @IBOutlet weak var lbRound: UILabel!
+    
+    
+    
+    @IBAction func onSliderValueChanged(_ sender: UISlider) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    @IBAction func onButtonOKClick(_ sender: UIButton) {
+    }
+    
+    @IBAction func onButtonInfoClick(_ sender: UIButton) {
+    }
+    
+    
 
 
 }
