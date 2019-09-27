fb79640c7b205e702ade4c5a73d45f6c8028110b
animevn <haanhth21@gmail.com>
2019-09-27 09:59:09 +0700

    update credit controller

---
+++ Bull/CreditController.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+class CreditController:UIViewController{
+    
+    @IBAction func onButtonBackClick(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
--- Bull/ViewController.swift
+++ Bull/ViewController.swift
@@ -11,18 +11,77 @@ class ViewController: UIViewController {
     @IBOutlet weak var lbRound: UILabel!
     
     
+    private var target = 0
+    private var guess = 0
+    private var min = 0
+    private var max = 100
+    private var score = 0
+    private var totalScore = 0
+    private var round = 0
+    private var info = ""
+    
+    private func setupSlider(){
+        sdSlider.setThumbImage(UIImage(named: "sliderThumbNormal"), for: .normal)
+        sdSlider.setThumbImage(UIImage(named: "sliderThumbHighlight"), for: .highlighted)
+        
+        let inset = UIEdgeInsets(top: 0, left: 12, bottom: 0, right: 12)
+        
+        let leftTrack = UIImage(named: "sliderTrackerLeft")
+        let leftSize = leftTrack!.resizableImage(withCapInsets: inset)
+        sdSlider.setMinimumTrackImage(leftSize, for: .normal)
+        
+        let rightTrack = UIImage(named: "sliderTrackerRight")
+        let rightSize = rightTrack!.resizableImage(withCapInsets: inset)
+        sdSlider.setMaximumTrackImage(rightSize, for: .normal)
+        
+        sdSlider.value = 50
+    }
+    
+    private func refreshInfo(){
+        info = ""
+        lbInfo.text = "\(info)"
+    }
+    
+    private func refreshLabels(){
+        sdSlider.value = 50
+        min = 0
+        lbMin.text = "\(min)"
+        max = 100
+        lbMax.text = "\(max)"
+        target = Int(arc4random_uniform(101))
+        lbTarget.text = "\(target)"
+    }
+    
+    private func resfreshTotal(){
+        round = 0
+        lbRound.text = "\(round)"
+        totalScore = 0
+        lbScore.text = "\(totalScore)"
+        
+    }
+    
+    private func reset(){
+        refreshLabels()
+        resfreshTotal()
+    }
     
     @IBAction func onSliderValueChanged(_ sender: UISlider) {
+        guess = Int(sender.value)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupSlider()
+        
+       
     }
     
     @IBAction func onButtonOKClick(_ sender: UIButton) {
+        
     }
     
     @IBAction func onButtonInfoClick(_ sender: UIButton) {
+        
     }
     
     
