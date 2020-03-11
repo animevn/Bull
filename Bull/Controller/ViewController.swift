d98ab27d35f68cbc0dcfab82c1398ffac6ee980b
animevn <haanhth21@gmail.com>
2020-03-11 21:58:09 +0700

    update to mvc, will need refract

--- Bull/Controller/ViewController.swift
+++ Bull/Controller/ViewController.swift
@@ -3,17 +3,18 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var lbTarget: UILabel!
-    @IBOutlet weak var lbMin: UILabel!
-    @IBOutlet weak var lbMax: UILabel!
     @IBOutlet weak var lbScore: UILabel!
     @IBOutlet weak var lbRound: UILabel!
     @IBOutlet weak var sdGuess: UISlider!
+    @IBOutlet weak var lbInfo: UILabel!
+    
+    var model = Model()
     
     private func setupSlider(){
         sdGuess.setThumbImage(UIImage(named: Base.sliderThumbNormal), for: .normal)
         sdGuess.setThumbImage(UIImage(named: Base.sliderThumbHightlight), for: .highlighted)
 
-        let inset = UIEdgeInsets(top: 0, left: 10, bottom: 0, right: 12)
+        let inset = UIEdgeInsets(top: 0, left: 15, bottom: 0, right: 15)
 
         let leftTrack = UIImage(named: Base.sliderTrackLeft)
         let leftSize = leftTrack?.resizableImage(withCapInsets: inset)
@@ -24,17 +25,54 @@ class ViewController: UIViewController {
         sdGuess.setMaximumTrackImage(rightSize, for: .normal)
     }
     
+    @IBAction func onSliderChange(_ sender: UISlider) {
+        model.updateGuess(guess: Int(sender.value))
+    }
+    
+    private func updateViews(){
+        lbInfo.text = model.info
+        lbScore.text = "\(model.totalScore)"
+        lbRound.text = "\(model.round)"
+        sdGuess.value = 50
+    }
+    
+    private func updateTarget(){
+        model.updateTarget()
+        lbTarget.text = "\(model.target)"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupSlider()
+        updateTarget()
+        updateViews()
     }
     
-
+    @IBAction func onButtonOKPressed(_ sender: UIButton) {
+        model.runGame()
+        updateViews()
+        updateTarget()
+    }
+    
+    private func showAlertWhenClickResetButton(){
+        let alert = UIAlertController(title: "Reset game",
+                                      message: "Your game data will be reset",
+                                      preferredStyle: .alert)
+        let actionOk = UIAlertAction(title: "OK",
+                                     style: .default,
+                                     handler: {_ in
+                                        self.model.reset()
+                                        self.updateTarget()
+                                        self.updateViews()
+        })
+        let actionCancel = UIAlertAction(title: "Cancel", style: .default, handler: nil)
+        alert.addAction(actionOk)
+        alert.addAction(actionCancel)
+        present(alert, animated: true, completion: nil)
+    }
+    
     @IBAction func bnReset(_ sender: UIButton) {
+        showAlertWhenClickResetButton()
     }
-    
-    @IBAction func bnInfo(_ sender: UIButton) {
-    }
-    
     
 }
--- Bull/Model/Model.swift
+++ Bull/Model/Model.swift
@@ -1,4 +1,5 @@
 import Foundation
+import UIKit
 
 struct Model{
     
@@ -7,7 +8,50 @@ struct Model{
     var score:Int = 0
     var totalScore:Int = 0
     var round:Int = 0
-    var info:String = ""
+    var info:String = "Waiting for your choice"
+    
+    mutating func updateTarget(){
+        target = Int.random(in: 0...101)
+    }
+    
+    mutating func reset(){
+        updateTarget()
+        guess = 0
+        score = 0
+        totalScore = 0
+        round = 0
+        info = "Waiting for your choice"
+    }
+    
+    mutating func updateGuess(guess:Int){
+        self.guess = guess
+    }
+    
+    private mutating func updateScore(){
+        if abs(guess - target) == 0{
+            score = 200
+        }else if abs(guess - target) == 1{
+            score = 90
+        }else if abs(guess - target) == 2{
+            score = 70
+        }else if abs(guess - target) == 3{
+            score = 50
+        }else if abs(guess - target) == 4{
+            score = 30
+        }else if abs(guess - target) == 5{
+            score = 10
+        }else {
+            score = 0
+        }
+    }
+    
+    mutating func runGame(){
+        updateScore()
+        totalScore += score
+        round += 1
+        info = "Your choice is \(guess) and your score is \(score)"
+    }
+    
     
     
     
