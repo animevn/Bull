e8c2d1994729a33dbd155a860d1e6e3c208cc09e
animevn <haanhth21@gmail.com>
2020-03-11 20:28:48 +0700

    add slider setup

--- Bull/Controller/ViewController.swift
+++ Bull/Controller/ViewController.swift
@@ -7,6 +7,27 @@ class ViewController: UIViewController {
     @IBOutlet weak var lbMax: UILabel!
     @IBOutlet weak var lbScore: UILabel!
     @IBOutlet weak var lbRound: UILabel!
+    @IBOutlet weak var sdGuess: UISlider!
+    
+    private func setupSlider(){
+        sdGuess.setThumbImage(UIImage(named: Base.sliderThumbNormal), for: .normal)
+        sdGuess.setThumbImage(UIImage(named: Base.sliderThumbHightlight), for: .highlighted)
+
+        let inset = UIEdgeInsets(top: 0, left: 10, bottom: 0, right: 12)
+
+        let leftTrack = UIImage(named: Base.sliderTrackLeft)
+        let leftSize = leftTrack?.resizableImage(withCapInsets: inset)
+        sdGuess.setMinimumTrackImage(leftSize, for: .normal)
+        
+        let rightTrack = UIImage(named: Base.sliderTrackRight)
+        let rightSize = rightTrack?.resizableImage(withCapInsets: inset)
+        sdGuess.setMaximumTrackImage(rightSize, for: .normal)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupSlider()
+    }
     
 
     @IBAction func bnReset(_ sender: UIButton) {
---
+++ Bull/Model/Base.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+struct Base{
+    
+    static let sliderThumbNormal:String = "sliderThumbNormal"
+    static let sliderThumbHightlight:String = "sliderThumbHighlight"
+    static let sliderTrackLeft:String = "sliderTrackerLeft"
+    static let sliderTrackRight:String = "sliderTrackerRight"
+    
+    
+}
