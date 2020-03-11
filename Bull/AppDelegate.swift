db171ac916f75b56fe1cd9ebb8c4276111706e02
animevn <haanhth21@gmail.com>
2020-03-11 17:37:35 +0700

    reset

--- Bull/AppDelegate.swift
+++ Bull/AppDelegate.swift
@@ -11,17 +11,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         didFinishLaunchingWithOptions launchOptions:[UIApplication.LaunchOptionsKey: Any]?)->Bool{
         
         window?.backgroundColor = .orange
-        
         return true
     }
 
+    func applicationDidBecomeActive(_ application: UIApplication) {}
+    
     func applicationWillResignActive(_ application: UIApplication) {}
 
     func applicationDidEnterBackground(_ application: UIApplication) {}
 
     func applicationWillEnterForeground(_ application: UIApplication) {}
-
-    func applicationDidBecomeActive(_ application: UIApplication) {}
 
     func applicationWillTerminate(_ application: UIApplication) {}
 
--- Bull/CreditController.swift
+++ Bull/CreditController.swift
@@ -2,7 +2,5 @@ import UIKit
 
 class CreditController:UIViewController{
     
-    @IBAction func onButtonBackClick(_ sender: UIButton) {
-        dismiss(animated: true, completion: nil)
-    }
+  
 }
